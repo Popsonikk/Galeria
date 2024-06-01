575d9dcc254fc566fc2f9f0ffe006e66042dd10e
Popsonikk <popson2001@onet.pl>
2024-06-01 10:43:23 +0200

    modele kolekcji

---
+++ models/gallery.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GallerySchema = new Schema({
+  name: { type: String, maxLength: 100, required: true },
+  description: { type: String, maxLength: 200 },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true }
+}, { collection: 'galleries' });
+
+// Export model
+module.exports = mongoose.model("Gallery", GallerySchema);
